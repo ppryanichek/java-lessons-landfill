d80d89778a9b9accac49b4e28cdf7c8e8a06a5b6
Eugene Pryazhnikov <kremen260@gmail.com>
2024-10-15 19:48:01 +0300

    added the removal of numbers from the string

--- Palindrome/palindrome.java
+++ Palindrome/palindrome.java
@@ -13,7 +13,7 @@ public class palindrome
     }
     
     public static boolean isPalindrome(String text) {
-        String newString = text.replaceAll("[^a-zA-Z]", "");
+        String newString = text.replaceAll("[^a-zA-Z0-9]", "");
 
         StringBuilder sb = new StringBuilder(newString);
         return newString.equalsIgnoreCase((sb.reverse().toString()));
