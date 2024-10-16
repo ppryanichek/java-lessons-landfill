5eb34a7fb716c47a4e96b3bc5de1d603bbb553bc
Eugene Pryazhnikov <kremen260@gmail.com>
2024-10-17 00:28:34 +0300

    program to remove duplicate elements from the array

---
+++ RemoveDuplicate/Main.java
@@ -0,0 +1,30 @@
+
+import java.util.Arrays;
+
+public class Main
+{
+     public static void main(String[] args) 
+     {
+         int[] arr = { 2, 4, 2, 4, 5, 3, 6, 2, 3, 6, 3 };
+         arr = removeDuplicates(arr, arr.length);
+         System.out.println(Arrays.toString(arr));
+     }
+
+    public static int[] removeDuplicates(int[] arr, int len)
+    {
+        Arrays.sort(arr);
+
+        int[] temparr = new int[len];
+        int count = 0;
+        for (int i = 0; i < len - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                temparr[count++] = arr[i];
+            }
+        }
+        
+        temparr[count++] = arr[len - 1];
+
+        return Arrays.copyOfRange(temparr, 0, count);
+    }
+}
+   
